f7e6093c67a5bdda8edcf7d83c1f15ce6a3dafe9
Smoot-Mena <Msmoot810@gmail.com>
2023-11-10 12:35:20 -0500

    Board Colors setup, Background and Gameboard Update

--- javascript/app.js
+++ javascript/app.js
@@ -2,11 +2,14 @@
  *  Global Variables
  **********************/
 
+/******Booleans******/
 let isHidden = false;
 let isFaceUp = false;
 let isDarkModeOn = false;
 let isGameActive = false;
 
+/***********Elements**********/
+let cards = document.querySelectorAll("div");
 let darkModeButton = document.querySelector("#dark-mode-button");
 let sun = document.querySelector("#sun");
 let clouds = document.querySelectorAll(".cloud");
@@ -17,6 +20,11 @@ let fourByFive = document.querySelector("#four-by-five");
 let fiveBySix = document.querySelector("#five-by-six");
 let sixBySix = document.querySelector("#six-by-six");
 let gameBoard = document.querySelector("#gameboard-container");
+let redButton = document.querySelector("#red");
+let greenButton = document.querySelector("#green");
+let blueButton = document.querySelector("#blue");
+let purpleButton = document.querySelector("#purple");
+let defaultButton = document.querySelector("#default");
 let resetButton = document.querySelector("#reset-button");
 // let saveSettings = localStorage;
 
@@ -52,7 +60,7 @@ function darkMode() {
 }
 
 function lightMode() {
-    document.body.style.backgroundImage = "linear-gradient(rgb(0, 250, 255), rgb(0, 150, 255),rgb(0, 205, 50), rgb(0, 155, 39))";
+    document.body.style.backgroundImage = "linear-gradient(rgb(0, 250, 255), rgb(0, 150, 255), rgb(0, 155, 39), rgb(146, 94, 63))";
     darkModeButton.style.backgroundColor = "yellow";
     darkModeButton.style.borderColor = "goldenrod";
     sun.style.backgroundImage = "radial-gradient(yellow, yellow, yellow, orange, orange)";
@@ -68,97 +76,72 @@ function lightMode() {
  *  Game Board
  ******************/
 
-/**********Board Size Functions*********/
+/**********Board Size Event Listeners*********/
 
 // 3 x 4
 threeByFour.addEventListener("click", () => {
     if (isGameActive === false) {
-        for (let row = 1; row <= 3; row++) {
-            for (let column = 1; column <= 4; column++) {
-                let card = document.createElement("div");
-                card.style.height = "100px";
-                card.style.width = "100px";
-                card.style.backgroundColor = "darkgrey";
-                card.style.zIndex = "1000";
-                card.style.margin = "3vh 1.3vw";
-                gameBoard.appendChild(card);
-                isGameActive = true;
-            }
-        }
+        createCards(3, 4);
     }
 });
 
 // 4 x 4
 fourByFour.addEventListener("click", () => {
     if (isGameActive === false) {
-        for (let row = 1; row <= 4; row++) {
-            for (let column = 1; column <= 4; column++) {
-                let card = document.createElement("div");
-                card.style.height = "90px";
-                card.style.width = "90px";
-                card.style.backgroundColor = "darkgrey";
-                card.style.zIndex = "1000";
-                card.style.margin = "1.2vh 1.5vw";
-                gameBoard.appendChild(card);
-                isGameActive = true;
-            }
-        }
+        createCards(4, 4);
     }
 });
 
 // 4 x 5
 fourByFive.addEventListener("click", () => {
     if (isGameActive === false) {
-        for (let row = 1; row <= 4; row++) {
-            for (let column = 1; column <= 5; column++) {
-                let card = document.createElement("div");
-                card.style.height = "80px";
-                card.style.width = "80px";
-                card.style.backgroundColor = "darkgrey";
-                card.style.zIndex = "1000";
-                card.style.margin = "1.1vh 1.8vw";
-                gameBoard.appendChild(card);
-                isGameActive = true;
-            }
-        }
+        createCards(4, 5);
     }
 });
 
 // 5 x 6
 fiveBySix.addEventListener("click", () => {
     if (isGameActive === false) {
-        for (let row = 1; row <= 5; row++) {
-            for (let column = 1; column <= 6; column++) {
-                let card = document.createElement("div");
-                card.style.height = "65px";
-                card.style.width = "65px";
-                card.style.backgroundColor = "darkgrey";
-                card.style.zIndex = "1000";
-                card.style.margin = "1vh 1.4vw";
-                gameBoard.appendChild(card);
-                isGameActive = true;
-            }
-        }
+        createCards(5, 6);
     }
 });
 
 // 6 x 6
 sixBySix.addEventListener("click", () => {
     if (isGameActive === false) {
-        for (let row = 1; row <= 6; row++) {
-            for (let column = 1; column <= 6; column++) {
-                let card = document.createElement("div");
-                card.style.height = "55px";
-                card.style.width = "55px";
-                card.style.backgroundColor = "darkgrey";
-                card.style.zIndex = "1000";
-                card.style.margin = "1.4vh 1.15vw";
-                gameBoard.appendChild(card);
-                isGameActive = true;
-            }
-        }
+        createCards(6, 6);
     }
 });
+
+
+/**********Board Color Event Listeners*********/
+
+redButton.addEventListener("click", () => {
+    gameBoard.style.borderColor = "red";
+    gameBoard.style.backgroundColor = "rgb(255, 185, 185)";
+});
+
+greenButton.addEventListener("click", () => {
+    gameBoard.style.borderColor = "green";
+    gameBoard.style.backgroundColor = "rgb(178, 252, 159)";
+});
+
+blueButton.addEventListener("click", () => {
+    gameBoard.style.borderColor = "blue";
+    gameBoard.style.backgroundColor = "rgb(159, 215, 252)";
+});
+
+purpleButton.addEventListener("click", () => {
+    gameBoard.style.borderColor = "purple";
+    gameBoard.style.backgroundColor = "rgb(219, 189, 254)";
+});
+
+defaultButton.addEventListener("click", () => {
+    gameBoard.style.borderColor = "darkgrey";
+    gameBoard.style.backgroundColor = "rgb(244, 244, 244)";
+});
+
+
 
 /************************
  *    Game Options
@@ -170,3 +153,48 @@ resetButton.addEventListener("click", () => {
     gameBoard.innerHTML = "";
     isGameActive = false;
 });
+
+/******************
+ *    Functions
+ *****************/
+
+function createCards(row, column) {
+    for (let x = 1; x <= row; x++) {
+        for (let y = 1; y <= column; y++) {
+            let card = document.createElement("div");
+            if (row == 3 && column == 4) {
+				card.style.height = "100px";
+				card.style.width = "100px";
+				card.style.zIndex = "1000";
+				card.style.margin = "3vh 1.3vw";
+			}
+            if (row == 4 && column == 4) {
+				card.style.height = "90px";
+                card.style.width = "90px";
+                card.style.zIndex = "1000";
+                card.style.margin = "1.2vh 1.5vw";
+			}
+            if (row == 4 && column == 5) {
+				card.style.height = "80px";
+                card.style.width = "80px";
+                card.style.zIndex = "1000";
+                card.style.margin = "1.1vh 1.8vw";
+			}
+            if (row == 5 && column == 6) {
+				card.style.height = "65px";
+                card.style.width = "65px";
+                card.style.zIndex = "1000";
+                card.style.margin = "1vh 1.4vw";
+			}
+            if (row == 6 && column == 6) {
+				card.style.height = "55px";
+                card.style.width = "55px";
+                card.style.zIndex = "1000";
+                card.style.margin = "1.4vh 1.15vw";
+			}
+            card.style.backgroundColor = "rgb(56, 56, 56)";
+            gameBoard.appendChild(card);
+            isGameActive = true;
+        }
+    }
+}
